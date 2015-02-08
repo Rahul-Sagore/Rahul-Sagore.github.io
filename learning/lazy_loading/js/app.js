8674392c94f3ee12d3e1a147350450527a6a3cff
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-08 16:59:19 +0530

    Added angular app name in app.js

--- learning/lazy_loading/js/app.js
+++ learning/lazy_loading/js/app.js
@@ -1,3 +1,3 @@
 //Instantiate an Angular JS module, module() acts as a setter & getter
 //app - module name, 2nd arg used to create app module as opposed to retrieving it
-angular.module('app', []);
+angular.module('lazyloader', []);
