9ad15abb0b91deeae00907ee06752d95f265d8e5
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-08 19:05:49 +0530

    Minor changes

--- learning/lazy_loading/js/main.ctrl.js
+++ learning/lazy_loading/js/main.ctrl.js
@@ -1,10 +1,13 @@
 //Retrive the app module, use the controller function to instantiate new controller
-angular.module('lazyloader').controller("MainController", function($scope,$http){
-   var url="https://api.instagram.com/v1/media/popular?access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201";
+/*angular.module('lazyloader').controller("MainController", function($scope,$http){
+   $http.defaults.useXDomain = true;
+   var url="https://api.instagram.com/v1/media/popular?client_id=9d918a36b4ea4648880a738450a4f5c4";
    $http.get(url).success( function(response) {
        $scope.instagram = response; 
-       console.log(response);
+    }).error(function(e){
+        console.log(e);
     });
    console.log($scope.instagram);
    $scope.p = "Hello";
 })
+*/
