2b259ff9fcd93bb79a48d71589e60850755f1a5c
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-08 17:26:09 +0530

    Updated Insta url with access token

--- learning/lazy_loading/js/main.ctrl.js
+++ learning/lazy_loading/js/main.ctrl.js
@@ -1,9 +1,9 @@
 //Retrive the app module, use the controller function to instantiate new controller
 angular.module('lazyloader').controller("MainController", function($scope,$http){
-   var url="https://api.instagram.com/oauth/authorize/?client_id=9d918a36b4ea4648880a738450a4f5c4&redirect_uri=REDIRECT-URI&response_type=code";
+   var url="https://api.instagram.com/v1/media/popular?access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201";
    $http.get(url).success( function(response) {
        $scope.instagram = response; 
     });
-   console.log(url);
+   console.log($scope.instagram);
    $scope.p = "Hello";
 })
