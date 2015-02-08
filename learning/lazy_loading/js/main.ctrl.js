6fbc7230c9367699aa1448a424782b21482127f2
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-08 17:28:13 +0530

    Minor changes console.log

--- learning/lazy_loading/js/main.ctrl.js
+++ learning/lazy_loading/js/main.ctrl.js
@@ -3,6 +3,7 @@ angular.module('lazyloader').controller("MainController", function($scope,$http)
    var url="https://api.instagram.com/v1/media/popular?access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201";
    $http.get(url).success( function(response) {
        $scope.instagram = response; 
+       console.log(response);
     });
    console.log($scope.instagram);
    $scope.p = "Hello";
