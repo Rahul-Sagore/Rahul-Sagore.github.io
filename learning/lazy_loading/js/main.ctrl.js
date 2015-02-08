0e70c8d244ed17bacb0adeed7bba474eb5a047c8
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-08 17:10:30 +0530

    Minor changes

--- learning/lazy_loading/js/main.ctrl.js
+++ learning/lazy_loading/js/main.ctrl.js
@@ -2,6 +2,8 @@
 angular.module('lazyloader').controller("MainController", function($scope,$http){
    var url="https://api.instagram.com/oauth/authorize/?client_id=9d918a36b4ea4648880a738450a4f5c4&redirect_uri=REDIRECT-URI&response_type=code";
    $http.get(url).success( function(response) {
-       $scope.students = response; 
+       $scope.instagram = response; 
     });
+   console.log(url);
+   $scope.p = "Hello";
 })
