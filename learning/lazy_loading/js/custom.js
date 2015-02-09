98051e94bcd261a6c70dd6160a650d62b8ecf5b6
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-09 13:50:21 +0530

    Mionr changes : load data boolean values

--- learning/lazy_loading/js/custom.js
+++ learning/lazy_loading/js/custom.js
@@ -2,9 +2,10 @@
   angular.module("lazyApp", [])
     .factory('instagram', ['$http',
         function($http) {
+
         	/*Function for making call to Instagram API for images*/
             return {
-                fetchPopular: function(tag, callback) {
+                fetchTag: function(tag, callback) {
                 	/*API with client ID of Rahul Sagore*/
                     if(typeof tag != "undefined"){
 	                    var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?callback=JSON_CALLBACK&count=20?client_id=9d918a36b4ea4648880a738450a4f5c4&access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201";
@@ -22,13 +23,19 @@
       $scope.pics = [];
       $scope.have = [];
       $scope.search = function(tag){
-      	/*Funcion for */
-      	$scope.pics = [];
-        $scope.have = [];
-      	$scope.getMore(tag);
+      	/*Funcion for removing previous search and searching new*/
+        if(typeof tag !== "undefined"){
+          $scope.dataLoading = true;
+          $scope.pics = [];
+          $scope.have = [];
+          $scope.getMore(tag);
+        }
+        else{
+          alert("Enter a search tag");
+        }
       }
       $scope.getMore = function(tag) {
-        instagram.fetchPopular(tag, function(data) {
+        instagram.fetchTag(tag, function(data) {
             for(var i=0; i<data.length; i++) {
               if (typeof $scope.have[data[i].id]==="undefined") {
                 $scope.pics.push(data[i]) ;
@@ -38,4 +45,5 @@
         });
       };
       $scope.getMore();
+
     });
