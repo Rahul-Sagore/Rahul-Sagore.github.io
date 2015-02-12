318d53fad7614ba134082886e09ea3e0bc514d64
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-12 17:46:55 +0530

    learn/lazy_loading/angular_lazy.html

--- learning/angular_demo/js/app.js
+++
@@ -1,3 +0,0 @@
-//Instantiate an Angular JS module, module() acts as a setter & getter
-//app - module name, 2nd arg used to create app module as opposed to retrieving it
-angular.module('app', []);
