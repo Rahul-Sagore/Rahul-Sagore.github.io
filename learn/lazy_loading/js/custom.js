cc162cc8786290b01f35c895f4570119155a91f0
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-13 18:45:14 +0530

    Update code next pagination in custom.js

--- learn/lazy_loading/js/custom.js
+++ learn/lazy_loading/js/custom.js
@@ -5,13 +5,14 @@
         function($http) {
         	/*Function for making call to Instagram API for images*/
             return {
-                fetchTag: function(tag, callback) {
+                fetchTag: function(tag, next, callback) {
                 	/*API with client ID of Rahul Sagore*/
                     if(typeof tag != "undefined"){
-	                    var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?callback=JSON_CALLBACK&count=20?client_id=9d918a36b4ea4648880a738450a4f5c4&access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201";
-	                    /* Making calls using $http object*/
+                      var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?callback=JSON_CALLBACK&count=20&client_id=9d918a36b4ea4648880a738450a4f5c4&access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201&max_id="+next+"";
+                      /* Making calls using $http object*/
 	                    $http.jsonp(url).success(function(response) {
-	                        callback(response.data);
+                          var next = response.pagination.next_max_id;
+	                        callback(response.data, next);
 	                    });
 	                }
                 }
@@ -34,8 +35,9 @@
           alert("Enter a search tag");
         }
       }
-      $scope.getMore = function(tag) {
-        instagram.fetchTag(tag, function(data) {
+      $scope.getMore = function(tag, next) {
+        instagram.fetchTag(tag, next, function(data, next) {
+            $scope.next = next;
             for(var i=0; i<data.length; i++) {
               if (typeof $scope.have[data[i].id]==="undefined") {
                 $scope.pics.push(data[i]) ;
@@ -66,7 +68,6 @@
     //custom directive for scrolling event
     return function(scope, elm, attr) {
         var raw = elm[0];
-        console.log(attr);
         var funCheckBounds = function(evt) {
             var rectObject = raw.getBoundingClientRect();
             if (rectObject.bottom === window.innerHeight) {
