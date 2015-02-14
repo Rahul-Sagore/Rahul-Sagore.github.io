5af9eb08ce1ab9350caa8207ca6a0a0dcc4df92b
Rahul-Sagore <rahul.sagore@gmail.com>
2015-02-14 11:28:33 +0530

    Removed unnecessary code

--- learn/lazy_loading/js/custom.js
+++ learn/lazy_loading/js/custom.js
@@ -6,8 +6,8 @@
         	/*Function for making call to Instagram API for images*/
             return {
                 fetchTag: function(tag, next, callback) {
-                	/*API with client ID of Rahul Sagore*/
-                    if(typeof tag != "undefined"){
+                	/*API with client ID of Rahul Sagore*/ 
+                    if(typeof tag != "undefined" && next != "stopped"){
                       var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?callback=JSON_CALLBACK&count=20&client_id=9d918a36b4ea4648880a738450a4f5c4&access_token=1696587752.9d918a3.eaf6363308074b069c37b52b6e085201&max_id="+next+"";
                       /* Making calls using $http object*/
 	                    $http.jsonp(url).success(function(response) {
@@ -21,14 +21,12 @@
     ])
     .controller("lazyController", function($scope, $http, $interval, instagram) {
       $scope.pics = [];
-      $scope.have = [];
       $scope.search = function(tag){
       	/*Funcion for removing previous search and searching new*/
         if(typeof tag !== "undefined"){
           $scope.loadMessage = "Loading...";
           $scope.pics = [];
-          $scope.have = [];
-          $scope.suggestion(tag);
+          $scope.getSuggestions(tag);
           $scope.getMore(tag);
         }
         else{
@@ -39,17 +37,15 @@
         instagram.fetchTag(tag, next, function(data, next) {
             $scope.next = next;
             for(var i=0; i<data.length; i++) {
-              if (typeof $scope.have[data[i].id]==="undefined") {
-                $scope.pics.push(data[i]) ;
-                $scope.have[data[i].id] = "1";
-              }
+                $scope.pics.push(data[i]);
             }
             if(next === undefined){
+              $scope.next = "stopped";
               $scope.loadMessage = "No image found to load";
             }
         });
       };
-      $scope.suggestion = function(tag){
+      $scope.getSuggestions = function(tag){
         //getting similar terms from JSON wordlist
         $scope.tag = tag; //Updating tag value based on suggestion
         tag_sliced = tag.slice(0,3).toLowerCase(); //Slicing first three letters from tag to match
@@ -65,8 +61,6 @@
               }
         });
       };
-      
-      $scope.getMore();
 }).directive('whenScrolled', function() {
     //custom directive for scrolling event
     return function(scope, elm, attr) {
@@ -76,8 +70,7 @@
             if (rectObject.bottom === window.innerHeight) {
                 scope.$apply(attr.whenScrolled);
             }
-        };
-        
+        };   
         angular.element(window).bind('scroll', funCheckBounds);
     };
 });
