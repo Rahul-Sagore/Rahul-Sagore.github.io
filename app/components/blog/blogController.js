f797fc7937ec69ef77c6c2c86a87747c564718e5
Rahul-Sagore <rahul.sagore@gmail.com>
2018-07-12 00:54:03 +0530

    Fix disqus identifier bug

--- app/components/blog/blogController.js
+++ app/components/blog/blogController.js
@@ -71,13 +71,13 @@
       $rootScope.SEO.title = post.title;
       $rootScope.SEO.description = post.title;
       setTimeout(function() {
-        DISQUS.reset({
-                reload: true,
-                config: function () {  
-                  this.page.identifier = post.url;  
-                  this.page.url = "http://rahul-sagore.github.io/#/blog/"+ post.url;
-                }
-              });
+        // DISQUS.reset({
+        //         reload: true,
+        //         config: function () {  
+        //           this.page.identifier = post.url;  
+        //           this.page.url = "http://rahul-sagore.github.io/#/blog/"+ post.url;
+        //         }
+        //       });
       }, 1000);
     }
 
